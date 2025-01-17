4b81c24d7d2890fb1132a3e2ec2b5ad313b7eb81
Thadeski11 <163975171+Thadeski11@users.noreply.github.com>
2025-01-17 14:24:59 -0300

    Update fuzzer.py

--- fuzzer.py
+++ fuzzer.py
@@ -1,6 +1,10 @@
 import requests
 import argparse
 from requests_ratelimiter import LimiterSession
+from requests.exceptions import RequestException
+import time
+
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
 
 parser = argparse.ArgumentParser(prog="Fuzzer", description="Script de fuzzing comum.")
 parser.add_argument("--url", help="Passar a url alvo.")
@@ -38,22 +42,25 @@ preparar_wordlist(abrir_arquivo)
 #Trata requisição por segundo
 session = LimiterSession(per_second=int(tempo_req))
 
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
-
-
 #Executa o script na url 
 def capturar_informacao(url):
+ status_force = [429, 500, 502, 503, 504]
  for linhas in arquivo_completo:
-  nova_url = url.replace("FUZZ", linhas) 
-  resposta = session.get(nova_url, headers=headers, timeout=10)
+  try:
+   nova_url = url.replace("FUZZ", linhas) 
+   resposta = session.get(nova_url, headers=headers, timeout=10)
 
-  status_code = resposta.status_code
-  tamanho_pagina = len(resposta.text)
+   status_code = resposta.status_code
+   tamanho_pagina = len(resposta.text)
   
-  
-  print(f"{nova_url} -- STATUS {status_code}  -  LENGHT {tamanho_pagina}")
+   print(f"{nova_url} -- STATUS {status_code}  -  LENGHT {tamanho_pagina}")
+
+   if status_code in status_force:
+    time.sleep(60)
+    
+  except requests.exceptions.RequestException as e:
+   print(f"Erro na url [{nova_url}] : {e}")
+   continue
 
  
-
 capturar_informacao(url_alvo)
-
