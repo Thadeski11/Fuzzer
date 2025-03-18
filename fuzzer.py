c29e67b33c7182b45e04285cc45a8d97d6878d58
Thadeski11 <163975171+Thadeski11@users.noreply.github.com>
2025-03-18 00:11:15 -0300

    Update fuzzer.py

--- fuzzer.py
+++ fuzzer.py
@@ -6,8 +6,8 @@ import threading
 import queue
 
 parser = argparse.ArgumentParser(prog="Fuzzer", description="Script de fuzzing comum.")
-parser.add_argument("--url", help="Passar a url alvo (Recomendado o uso de ' ') Ex: 'https://google.com/FUZZ/?family=FUZZ'.")
-parser.add_argument("--wordlist", help="Passar a payload de testes.")
+parser.add_argument("--url", required=True, help="Passar a url alvo (Recomendado o uso de ' ') Ex: 'https://google.com/FUZZ/?family=FUZZ'.")
+parser.add_argument("--wordlist", required=True, help="Passar a payload de testes.")
 parser.add_argument("--time", type=float, default=1, help="Para baixas taxas de requisição.")
 parser.add_argument("--threads", type=int, help="Para altas taxas de requisição.")
 parser.add_argument("--output", help="Salva resultados em um arquivo.")
