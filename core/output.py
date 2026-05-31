2962b6b377eb05b831c3307922a0028201404ee2
Thadeski11 <163975171+Thadeski11@users.noreply.github.com>
2026-05-31 17:18:41 -0300

    Create output.py

---
+++ core/output.py
@@ -0,0 +1,42 @@
+import json
+import xml.etree.ElementTree as ET
+import csv
+
+class Output():
+	def __init__(self, results, file):
+		self.results = results
+		self.output_file = file
+	
+
+	def text_file(self):
+		with open(self.output_file, "w") as o:
+			if "body" in self.results[0]:
+				for lines in self.results:
+					o.write(f"{lines['url']:<100} {lines['status_code']:>10} {lines['length']:>10} {lines['body']:>20}\n")	
+			else:
+				for lines in self.results:
+					o.write(f"{lines['url']:<100} {lines['status_code']:>10} {lines['length']:>10}\n")
+
+	def json_file(self):
+		with open(self.output_file, "w") as o:
+			o.write(json.dumps(self.results, indent=4))
+
+
+	def xml_file(self):
+		root = ET.Element("results")
+
+		for item in self.results:
+			result = ET.SubElement(root, "result")
+			for key, value in item.items():
+				element = ET.SubElement(result, key)
+				element.text = str(value)
+
+		tree = ET.ElementTree(root)
+		tree.write(self.output_file, encoding="UTF-8", xml_declaration=True)
+
+
+	def csv_file(self):
+		with open(self.output_file, "w") as o:
+			writer = csv.DictWriter(o, fieldnames=self.results[0].keys())
+			writer.writeheader()
+			writer.writerows(self.results)
